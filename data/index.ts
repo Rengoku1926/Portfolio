f11b5805a8b2155dd002f25aa06665e21cc6136c
Prateek <prateekmahapatra1926@gmail.com>
2024-09-03 22:46:40 +0530

    all changes done

---
+++ data/index.ts
@@ -0,0 +1,190 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a Customizable test app",
+      description: "Pariksha",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "ChatGPT Clone",
+      des: "An AI chatbot that utilizes the 3.5model of openAI",
+      img: "/chatbot.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+      link: "/ui.earth.com",
+    },
+    {
+      id: 2,
+      title: "Promptopia",
+      des: "An AI prompt sharing app to share and create new interesting prompts with tags to view different users.",
+      img: "/promptopia.png",
+      iconLists: ["/next.svg", "/tail.svg", "/js.svg","/c.svg"],
+      link: "https://ai-prompt-app-h1fo.vercel.app/",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Prateek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prateek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Prateek is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Prateek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prateek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Prateek is the ideal partner..",
+      name: "Michael Johnson",
+      title: "Director of Random Company",
+    },
+    {
+      quote:
+        "Collaborating with Prateek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prateek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Prateek is the ideal partner. .",
+      name: "Michael Johnson",
+      title: "Director of Random Company",
+    },
+    {
+      quote:
+        "Collaborating with Prateek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prateek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Prateek is the ideal partner. .",
+      name: "Michael Johnson",
+      title: "Director of Random Company",
+    },
+    {
+      quote:
+        "Collaborating with Prateek was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Prateek's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Prateek is the ideal partner. .",
+      name: "Michael Johnson",
+      title: "Director of Random Company",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "figma",
+      img: "/Figma-logo.svg.png",
+      nameImg: "Figma",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Web developer",
+      desc: "Developed the website of a new media agency promoting digital marketing.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Freelancer",
+      desc: "Did some freelance projects on upwork and freelancer",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/prateek-mahapatra-013ba1249"
+    },
+    {
+      id: 2,
+      img: "/git.svg",
+      link:"https://github.com/Rengoku1926"
+    },
+    {
+      id: 3,
+      img: "/twit.svg",
+      link: "https://x.com/PrateekMahapa14"
+    },
+  ];
