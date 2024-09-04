5f2eff22404afdc08cbc095ba97818ea0e7fe18d
Prateek <prateekmahapatra1926@gmail.com>
2024-09-04 16:52:57 +0530

    new bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5 ">
     <div className=" max-w-7xl w-full">
-      <h1> Hello fam : </h1>
+      <h1> </h1>
       <FloatingNav navItems={navItems} />
       <Hero />
       <Grid />
